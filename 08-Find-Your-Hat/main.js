78e7dd048d6ff8f9bb12513dce31a2518ff07a8c
efgarro <efgarro@yahoo.com>
2022-01-22 13:57:35 -0600

    draft Find you Hat

--- 08-Find-Your-Hat/main.js
+++ 08-Find-Your-Hat/main.js
@@ -1,6 +1,24 @@
-const prompt = require('prompt-sync')({sigint: true});
+const prompt = require("prompt-sync")({ sigint: true });
 
-const hat = '^';
-const hole = 'O';
-const fieldCharacter = '░';
-const pathCharacter = '*';
+const hat = "^";
+const hole = "O";
+const fieldCharacter = "░";
+const pathCharacter = "*";
+
+class Field {
+    constructor(twoDArr) {}
+
+    print() {
+
+    }
+
+    static generateField() {
+        
+    }
+}
+
+const myField = new Field([
+    ["*", "░", "O"],
+    ["░", "O", "░"],
+    ["░", "^", "░"],
+]);
